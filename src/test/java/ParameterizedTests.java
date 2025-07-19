ed5515af42beb22a1a864d38532db8176037147a
Anastasiya Elkafrawi <atverdova78@gmail.com>
2025-07-19 14:06:45 +0400

    some fixes: Naming corrected AfterEach added

--- src/test/java/ParameterizedTests.java
+++ src/test/java/ParameterizedTests.java
@@ -1,7 +1,9 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import data.Applications;
 import data.Language;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
@@ -17,10 +19,15 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class ParameterizedTests {
 
-    @AfterEach
-    public void OpenBrowser() {
+    @BeforeEach
+    public void openBrowser() {
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
+    }
+
+    @AfterEach
+    void afterEach() {
+        Selenide.closeWebDriver();
     }
 
     @ValueSource(ints = {
@@ -28,7 +35,7 @@ public class ParameterizedTests {
     })
     @ParameterizedTest(name = "Квадратный корень числа {0} получен корректно")
     @Tag("MATH")
-    public void calcsetSiteCalculatesSquareRootCorrectly(int number){
+    public void calcsetSiteCalculatesSquareRootCorrectlyTest(int number){
         open("https://calcset.ru/calculator.php?category=math&calculator=square_root_calculator#result");
         $("input#number").setValue(String.valueOf(number)).pressEnter();
         $("div#result").shouldHave(text(String.valueOf(Math.sqrt(number))));
@@ -42,7 +49,7 @@ public class ParameterizedTests {
 
     @ParameterizedTest(name = "Для поискового запроса {0} первым результатом отображен текст {1}")
     @Tag("SMOKE")
-    public void sstuSearchShowsArticlesCorrect(String searchQuery, String expectedText) {
+    public void sstuSearchShowsArticlesCorrectTest(String searchQuery, String expectedText) {
         open("https://www.sstu.ru/");
         $(".fa-search").click();
         $("#inline-search__input").setValue(searchQuery).pressEnter();
@@ -52,13 +59,13 @@ public class ParameterizedTests {
     @EnumSource(Applications.class)
     @ParameterizedTest(name = "Для скачивания приложения {0} кнопка отображена")
     @Tag("SMOKE")
-    void rzdSiteShouldDisplayApplicationsSet(Applications apps){
+    void rzdSiteShouldDisplayApplicationsSetTest(Applications apps){
     open("https://www.rzd.ru/");
         $$("[data-test-id='footer_contacts_container'] > li > a").shouldHave(size(5));
                 $("[title='" + apps.applicationName + "']").shouldBe(visible);
     }
 
-    static Stream<Arguments> sheremetyevoSiteHaveCorrectMenuItemsOnEngAndRus() {
+    static Stream<Arguments> sheremetyevoSiteHaveCorrectMenuItemsOnEngAndRusTest() {
         return Stream.of(
                 Arguments.of(Language.RU, List.of("Вылет", "Прилет", "Пересадка")),
                 Arguments.of(Language.ENG, List.of("Departures", "Arrivals", "Connections"))
@@ -68,7 +75,7 @@ public class ParameterizedTests {
     @MethodSource
     @ParameterizedTest(name = "На языке {0} отображены пункты меню {1}")
     @Tag("SMOKE")
-    void sheremetyevoSiteHaveCorrectMenuItemsOnEngAndRus(Language language, List<String> menuItems) {
+    void sheremetyevoSiteHaveCorrectMenuItemsOnEngAndRusTest(Language language, List<String> menuItems) {
         open("https://www.svo.aero/en/main");
         $("#mat-select-0").click();
         $$(".mat-option-text").findBy(text(language.languageName)).click();
